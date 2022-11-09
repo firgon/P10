03b741daaf426cbf3d427289aedce186b408380a
Emmanuel Albisser <emmanuel.albisser@gmail.com>
2022-11-09 18:50:50 +0100

    .env ok

--- softDesk/settings.py
+++ softDesk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b+#1b5&qno8f(x2x*b9k!$z(t6o4b3xs^6_8+xb%%*7l%1af^g'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
