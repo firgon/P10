bc9809ef8a055c377ba2c77ff8f4903d64e468cd
Emmanuel Albisser <emmanuel.albisser@gmail.com>
2022-11-13 20:58:37 +0100

    data sent after update

--- restAPI/views/generics_views.py
+++ restAPI/views/generics_views.py
@@ -131,7 +131,7 @@ class ManagingGenericAPIViewForSoftDesk(GenericAPIViewForSoftDesk):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
